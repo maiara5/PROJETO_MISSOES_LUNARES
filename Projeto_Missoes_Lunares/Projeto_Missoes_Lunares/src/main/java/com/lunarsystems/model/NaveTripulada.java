1749d4c783b504fc98800c7797c51a18f5299d59
gabrielle-saito <miygabrielle@gmail.com>
2025-12-02 17:55:20 -0300

    Add comments to NaveTripulada class
    Added comments to clarify the purpose of the class and its methods.

--- Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/model/NaveTripulada.java
+++ Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/model/NaveTripulada.java
@@ -1,14 +1,15 @@
 package com.lunarsystems.model;
 
 public class NaveTripulada extends Nave {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; //serialização
 
     public NaveTripulada() {}
 
     public NaveTripulada(String id, String nome, int capacidadeTripulantes) {
-        super(id, nome, capacidadeTripulantes);
+        super(id, nome, capacidadeTripulantes); //em vez de repetir código para inicializar delega pra superclasse
     }
 
     @Override
-    public String getTipo() { return "Tripulada"; }
+    public String getTipo() { return "Tripulada"; } //se referência tipo Nave apontando para uma NaveTripulada e chamar getTipo() retorna tripulada
 }
+
