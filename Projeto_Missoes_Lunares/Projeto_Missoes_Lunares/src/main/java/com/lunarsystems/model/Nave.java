224c29bb8db285e539b306b44728460c787949c0
gabrielle-saito <miygabrielle@gmail.com>
2025-12-02 16:08:29 -0300

    Create abstract class Nave for spacecraft model

---
+++ Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/model/Nave.java
@@ -0,0 +1,33 @@
+public abstract class Nave implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected String id;
+    protected String nome;
+    protected int capacidadeTripulantes;
+
+    public Nave() {}
+
+    public Nave(String id, String nome, int capacidadeTripulantes) {
+        this.id = id;
+        this.nome = nome;
+        this.capacidadeTripulantes = capacidadeTripulantes;
+    }
+
+    // Getters e Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public int getCapacidadeTripulantes() { return capacidadeTripulantes; }
+    public void setCapacidadeTripulantes(int capacidadeTripulantes) { this.capacidadeTripulantes = capacidadeTripulantes; }
+
+    public abstract String getTipo();
+
+    @Override
+    public String toString() {
+        return String.format("Nave[id=%s, nome=%s, tipo=%s, capacidade=%d]",
+                id, nome, getTipo(), capacidadeTripulantes);
+    }
+}
