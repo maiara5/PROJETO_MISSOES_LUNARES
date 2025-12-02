ef909a05e4c9d1e71f4ae4ef8424003cda1e09c9
Maiara Marques Ferreira <maiara5@unochapeco.edu.br>
2025-12-02 16:28:03 -0300

    Update NitriteRepository.java

---
+++ Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/repository/NitriteRepository.java
@@ -0,0 +1,44 @@
+package com.lunarsystems.repository;
+
+import com.lunarsystems.model.Missao;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.dizitart.no2.objects.filters.ObjectFilters.*;
+
+public class NitriteRepository implements AutoCloseable {
+    private final Nitrite db;
+    private final ObjectRepository<Missao> repo;
+
+    public NitriteRepository(Path file) {
+        this.db = Nitrite.builder()
+                .filePath(file.toString())
+                .openOrCreate();
+        this.repo = db.getRepository(Missao.class);
+    }
+
+    public void salvarMissao(Missao m) {
+        repo.insert(m); 
+    }
+
+    public List<Missao> listarMissao() {
+        return repo.find().toList();
+    }
+
+    public Missao buscarPorCodigo(String codigo) {
+        return repo.find(eq("codigo", codigo)).firstOrDefault();
+    }
+
+    /**
+     */
+    @Override
+    public void close() {
+        if (!db.isClosed()) {
+            db.commit(); 
+            db.close();
+        }
+    }
+}
