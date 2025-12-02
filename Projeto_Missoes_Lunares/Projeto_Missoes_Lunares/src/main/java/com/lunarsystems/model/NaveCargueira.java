181b29157c3f01d27f81e2e383fce8b08fb66d13
gabrielle-saito <miygabrielle@gmail.com>
2025-12-02 17:50:07 -0300

    Add comments to NaveCargueira class
    Added comments to clarify class functionality.

--- Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/model/NaveCargueira.java
+++ Projeto_Missoes_Lunares/Projeto_Missoes_Lunares/src/main/java/com/lunarsystems/model/NaveCargueira.java
@@ -1,14 +1,15 @@
 package com.lunarsystems.model;
 
 public class NaveCargueira extends Nave {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; //transformar objeto em bytes para salvar/enviar
 
-    public NaveCargueira() {}
+    public NaveCargueira() {} //criar pra preencher depois
 
-    public NaveCargueira(String id, String nome, int capacidadeTripulantes) {
-        super(id, nome, capacidadeTripulantes);
-    }
+    public NaveCargueira(String id, String nome, int capacidadeTripulantes) { 
+        super(id, nome, capacidadeTripulantes); //Chama o construtor da superclasse passando esses valores
+    } 
 
     @Override
-    public String getTipo() { return "Cargueira"; }
+    public String getTipo() { return "Cargueira"; } //devolve o tipo da nave como Cargueira
 }
+
